3141c4d4ce2f693d67b50b35488e6aac1275d000
andrewjacob01 <andrew.jac.var@gmail.com>
2022-06-04 16:02:12 -0600

    Update actor.py

--- game/actor.py
+++ game/actor.py
@@ -23,6 +23,7 @@ class Actor:
         self._color = Color(255, 255, 255)
         self._position = Point(0, 0)
         self._velocity = Point(0, 0)
+        self._points = ""
 
     def get_color(self):
         """Gets the actor's color as a tuple of three ints (r, g, b).
@@ -115,3 +116,14 @@ class Actor:
             velocity (Point): The given velocity.
         """
         self._velocity = velocity
+
+    def get_points(self, points):
+        
+
+        self._points = points
+
+    def set_points(self, points):
+
+
+        self._points = points
+        
